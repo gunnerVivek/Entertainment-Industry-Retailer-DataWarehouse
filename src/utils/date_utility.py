f9137613e7fd179e2030aefc4e627f189bdef58e
MY-PC <vivpur88@gmail.com>
2022-01-05 15:02:14 +0530

    feat: Adds suport for specifying time zone

--- src/utils/date_utility.py
+++ src/utils/date_utility.py
@@ -1,7 +1,10 @@
-from datetime import date
+#TODO: Handle value errors
+
+from datetime import date, datetime
+import pytz
 
 
-def get_current_date(format='%Y%m%d')->str:
+def get_current_date(format='%Y%m%d', time_zone='UTC')->str:
     '''
         This method is used to get the current date in the specifed format.
         If the provided date format is not specified, it defaults to %Y%m%d.
@@ -9,47 +12,60 @@ def get_current_date(format='%Y%m%d')->str:
 
         Parameters
         ----------------
-        format: Format of the date to be returned.
+        format   : Format of the date to be returned.
+        time_zone: Time zone of the date to be returned.
 
         Returns
         ----------------
         date string in the specified format.
     '''
     try:
-        return date.today().strftime(format)
+        return datetime.now(pytz.timezone(time_zone)).date().strftime(format)
+
     except ValueError:
         # TODO:log
-        ''
-    finally:
-        return date.today().strftime("%Y%m%d")
+        return datetime.now(pytz.timezone('UTC')).date().strftime("%y%m%d")
 
 
-def get_current_time(format='%H%M%S')->str:
+def get_current_time(format='%H%M%S', time_zone='UTC')->str:
     '''
         This method is used to get the current time in the specifed format.
+        If the provided time format is not specified, it defaults to %H%M%S.
+        In case specified format or time zone is not supported, it fallsback to defaults.
+
+        Parameters
+        ----------------
+        format   : Format of the time to be returned.
+        time_zone: Time zone of the time to be returned.
+
+        Returns
+        ----------------
+        time string in the specified format. 
     '''
     try:
-        return date.today().strftime(format)
+        return datetime.now(pytz.timezone(time_zone)).time().strftime(format)
+        
     except ValueError:
-        ''
-    finally:
-        return date.today().strftime("%H%M%S")
+        return datetime.now(pytz.timezone('UTC')).time().strftime('%H%M%S')
+        
 
-
-def get_current_date_time(format='%Y%m%d%H%M%S')->str:
+def get_current_date_time(format='%Y%m%d%H%M%S', time_zone='UTC')->str:
     '''
         This method is used to get the current date and time in the specifed format.
+        If the provided date format is not specified, it defaults to %Y%m%d.
+        In case specified format is not supported, it fallsback to defaults.
+
+        Parameters
+        ----------------
+        format   : Format of the date and time to be returned.
+        time_zone: Time zone of the date and time to be returned.
+
+        Returns
+        ----------------
+        date and time string in the specified format.
     '''
-
     try:
-        return date.today().strftime(format)
+        return datetime.now(pytz.timezone(time_zone)).strftime(format)
+
     except ValueError:
-        ''
-    finally:
-        return date.today().strftime("%Y%m%d%H%M%S")
-
-
-# if __name__ == '__main__':
-#     print(get_current_date('%q'))
-#     # print(get_current_time())
-#     # print(get_current_date_time())
+        return datetime.now(pytz.timezone('UTC')).strftime('%Y%m%d%H%M%S')
